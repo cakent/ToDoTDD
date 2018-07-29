adcec9efad468bc2a3258883f78c46078e3646fa
c kent <cakent@memphis.edu>
2018-07-29 15:32:49 -0500

    Added add task screen. Updates when text in Edit Text is updated. Working on Robolectric tests

--- AddTaskFragment.java
+++ AddTaskFragment.java
@@ -2,11 +2,18 @@ package com.example.ckent.todotdd;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class AddTaskFragment extends Fragment {
+
+    String title = null;
+    String desc = null;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -15,8 +22,49 @@ public class AddTaskFragment extends Fragment {
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
         View v = inflater.inflate(R.layout.addtask_fragment,container,false);
+        final EditText titleText =(EditText) v.findViewById(R.id.titleEditText);
+        final EditText detailText = (EditText) v.findViewById(R.id.detailEditText);
+        Button save =(Button) v.findViewById(R.id.addTaskButton);
+        titleText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
 
+            }
 
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                title=titleText.toString();
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
+        detailText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                desc = detailText.toString();
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
+        save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Tasks task = new Tasks(title,desc);
+            }
+        });
 
         return v;
     }
--- SingleFragmentActivity.java
+++ SingleFragmentActivity.java
@@ -11,7 +11,7 @@ import android.support.v7.app.AppCompatActivity;
  */
 
 public abstract class SingleFragmentActivity extends AppCompatActivity {
-    protected abstract TaskDetailFragment createFragment();
+    protected abstract Fragment createFragment();
 
     @LayoutRes
     protected int getLayoutResId(){
